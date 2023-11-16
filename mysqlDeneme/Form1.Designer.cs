94620b0341fce09d832fdb66bd6139a07258a1bf
yavuzakan <yavuz.akan@gmail.com>
2023-11-16 12:24:45 +0300

    Proje dosyası ekle.

---
+++ mysqlDeneme/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace mysqlDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(221, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(376, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(376, 126);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(376, 152);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.user1,
+            this.user2,
+            this.user3});
+            this.dataGridView1.Location = new System.Drawing.Point(2, 245);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(612, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            // 
+            // user1
+            // 
+            this.user1.HeaderText = "user1";
+            this.user1.Name = "user1";
+            // 
+            // user2
+            // 
+            this.user2.HeaderText = "user2";
+            this.user2.Name = "user2";
+            // 
+            // user3
+            // 
+            this.user3.HeaderText = "user3";
+            this.user3.Name = "user3";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(506, 39);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(491, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(491, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ mysqlDeneme/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace mysqlDeneme
+{
+    public partial class Form1 : Form
+    {
+        string myConnectionString = "server=localhost;database=ctry;uid=yavuz;pwd=123456789;";
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.dataGridView1.AllowUserToAddRows = false;
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dataGridView1.ReadOnly = true;
+            ara();
+
+        }
+       
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "ctry",
+                UserID = "yavuz",
+                Password = "123456789",
+
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                string user1 = textBox1.Text;
+                string user2 = textBox2.Text;
+                try
+                {
+                    conn.Open();
+                    //MessageBox.Show("Connection Open!");
+
+                    using (var command = conn.CreateCommand())
+                    {
+
+                        command.CommandText = "SELECT * FROM user where user1 LIKE @user1 and user2 LIKE @user2 ";
+
+                        command.Parameters.AddWithValue("@user1", user1);
+                        command.Parameters.AddWithValue("@user2", user2);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            label1.Text = "2";
+                            while (reader.Read())
+                            {
+
+                                MessageBox.Show(reader.GetString(1).ToString());
+
+                            }
+                            label1.Text = "4";
+                        }
+                    }
+
+
+
+
+
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+            /*
+            string user1 = textBox1.Text;
+            string user2 = textBox2.Text;
+
+            MessageBox.Show(user2);
+
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+            try
+            {
+                cnn.Open();
+                // MessageBox.Show("Connection Open!");
+
+
+
+
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
+            */
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "ctry",
+                UserID = "yavuz",
+                Password = "123456789",
+
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                string user1 = textBox3.Text;
+                string user2 = textBox4.Text;
+                string user3 = textBox5.Text;
+                try
+                {
+                    conn.Open();
+                    //MessageBox.Show("Connection Open!");
+
+                    using (var command = conn.CreateCommand())
+                    {
+
+                        command.CommandText = "INSERT INTO  user(user1 , user2 , user3 ) VALUES (@user1 , @user2 , @user3)";
+
+                        command.Parameters.AddWithValue("@user1", user1);
+                        command.Parameters.AddWithValue("@user2", user2);
+                        command.Parameters.AddWithValue("@user3", user3);
+
+
+                        command.ExecuteNonQuery();
+                        textBox3.Text = "";
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                    }
+
+
+
+
+
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+        }
+
+        public void ara()
+        {
+
+            dataGridView1.Rows.Clear();
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "ctry",
+                UserID = "yavuz",
+                Password = "123456789",
+
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                string user1 = textBox1.Text;
+                string user2 = textBox2.Text;
+                try
+                {
+                    conn.Open();
+                    //MessageBox.Show("Connection Open!");
+
+                    using (var command = conn.CreateCommand())
+                    {
+
+                        command.CommandText = "SELECT * FROM user ";
+
+
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            label1.Text = "2";
+                            while (reader.Read())
+                            {
+
+                                // MessageBox.Show(reader.GetString(1).ToString());
+                                dataGridView1.Rows.Insert(0, reader.GetValue(0).ToString(), reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), reader.GetValue(3).ToString());
+                            }
+                            label1.Text = "4";
+                        }
+                    }
+
+
+
+
+
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow dataGridViewRow = dataGridView1.Rows[e.RowIndex];
+
+                textBox3.Text = dataGridViewRow.Cells["user1"].Value.ToString();
+                textBox4.Text = dataGridViewRow.Cells["user2"].Value.ToString();
+                textBox5.Text = dataGridViewRow.Cells["user3"].Value.ToString();
+
+                textBox6.Text = dataGridViewRow.Cells["id"].Value.ToString();
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "ctry",
+                UserID = "yavuz",
+                Password = "123456789",
+
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                string user1 = textBox3.Text;
+                string user2 = textBox4.Text;
+                string user3 = textBox5.Text;
+                string id = textBox6.Text;
+                try
+                {
+                    conn.Open();
+                    //MessageBox.Show("Connection Open!");
+
+                    using (var command = conn.CreateCommand())
+                    {
+
+                        command.CommandText = "UPDATE user set user1=@user1, user2=@user2 , user3=@user3 WHERE id=@id";
+
+                        command.Parameters.AddWithValue("@user1", user1);
+                        command.Parameters.AddWithValue("@user2", user2);
+                        command.Parameters.AddWithValue("@user3", user3);
+                        command.Parameters.AddWithValue("@id", id);
+
+                        command.ExecuteNonQuery();
+                        textBox3.Text = "";
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                        ara();
+                    }
+
+
+
+
+
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "ctry",
+                UserID = "yavuz",
+                Password = "123456789",
+
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                string user1 = textBox3.Text;
+                string user2 = textBox4.Text;
+                string user3 = textBox5.Text;
+                string id = textBox6.Text;
+                try
+                {
+                    conn.Open();
+                    //MessageBox.Show("Connection Open!");
+
+                    using (var command = conn.CreateCommand())
+                    {
+
+                        command.CommandText = "DELETE FROM user WHERE id=@id";
+
+                     
+                        command.Parameters.AddWithValue("@id", id);
+
+                        command.ExecuteNonQuery();
+                        textBox3.Text = "";
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                        ara();
+                    }
+
+
+
+
+
+
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
+        }
+    }
+}
